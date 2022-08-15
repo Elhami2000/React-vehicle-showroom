6cd57785207758b6786a9b9f88be512521ec1b28
Elhami2000 <73657625+Elhami2000@users.noreply.github.com>
2022-08-15 18:37:58 +0200

    Added Shopping products list

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -96,7 +96,7 @@ const Slider = () => {
             {sliderItems.map(item=>(
         <Slide bg={item.bg}>
             <ImgContainer>
-                <Image src={item.img} />
+                <Image src={item.img} key={item.id} />
             </ImgContainer>
             <InfoContainer>
                 <Title>{item.title}</Title>
